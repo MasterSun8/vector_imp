d80dfbe0b2429902817d28b75c7aa4de654339c6
MasterSun8 <kacpi.doro@gmail.com>
2022-04-12 20:51:53 +0200

    example

--- vec.cpp
+++ vec.cpp
@@ -3,6 +3,12 @@
 
 int main(){
     myVector x(10);
-    x.insert(9, 110);
+    x.pushBack(10);
+    x.printVector();
+    x.popBack();
+    x.printVector();
+    x.insert(3, 1000);
+    x.printVector();
+    x.remove(5);
     x.printVector();
 }
